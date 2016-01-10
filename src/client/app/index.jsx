b0b0030ceb00d67efe192b84d43959b9bb899689
Todd Kronenberg <toddkronenberg@gmail.com>
2016-01-09 23:05:44 -0600

    Added localStorage operations to keep data.

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
 import {render} from 'react-dom';
+import classNames from 'classnames';
 import CreateList from './createList.jsx';
 import ShowList from './showList.jsx';
 
 let AppComponent = React.createClass({
   getInitialState() {
-    return { list: [] };
+    return { list: window.localStorage.getItem('dailyDo') ? JSON.parse(window.localStorage.getItem('dailyDo')) : [] };
   },
 
   showList(list) {
+    window.localStorage.setItem('dailyDo', JSON.stringify(list));
     this.setState({list: list});
   },
 
   completeItem(itemId) {
     let tempList = this.state.list;
     tempList[itemId].completed = true;
+    window.localStorage.setItem('dailyDo', JSON.stringify(tempList));
     this.setState({list: tempList});
   },
 
   render() {
+    let hideIfList = classNames({'hide': this.state.list.length});
+    let hideIfNoList = classNames({'hide': !this.state.list.length});
     return (
       <div>
-        <CreateList showList={this.showList} />
-        <ShowList list={this.state.list} completeItem={this.completeItem} />
+        <div className={hideIfList}>
+          <CreateList showList={this.showList} />
+        </div>
+        <div className={hideIfNoList}>
+          <ShowList list={this.state.list} completeItem={this.completeItem} />
+        </div>
       </div>
     );
   }
--- src/client/app/showList.jsx
+++ src/client/app/showList.jsx
@@ -12,13 +12,14 @@ let ShowList = React.createClass({
     document.getElementById('done-button').classList.add('hide');
     document.getElementById('results').classList.remove('hide');
     this.setState({resultsShown: true});
+    window.localStorage.removeItem('dailyDo');
   },
 
   render() {
     let list = this.props.list.map((item) => <ShowListItem item={item} key={item.id} completeItem={this.props.completeItem} resultsShown={this.state.resultsShown} />);
     let numberCompleted = this.props.list.filter((item) => item.completed).length
     return (
-      <div id='showList' className='hide'>
+      <div id='showList'>
         <h1 className='text-center'> Todays List </h1>
         <ul className='text-center'>
           {list}
